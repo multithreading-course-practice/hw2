03c8ec96d2fff928a4ddedf7fcb765a6517f9d3b
andy spinko <spdale016@gmail.com>
2024-07-08 22:36:28 +0300

    hw2 is done

--- src/main/java/org/example/Warehouse.java
+++ src/main/java/org/example/Warehouse.java
@@ -1,17 +1,20 @@
 package org.example;
 
-import lombok.Getter;
-import lombok.extern.log4j.Log4j2;
-
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Queue;
+
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Getter
 public class Warehouse extends Thread {
 
     private final List<Block> storage = new ArrayList<>();
+    private final Queue<Truck> trucks = new ArrayDeque<>();
 
     public Warehouse(String name) {
         super(name);
@@ -26,13 +29,18 @@ public class Warehouse extends Thread {
     public void run() {
         Truck truck;
         while (!currentThread().isInterrupted()) {
-            truck = getNextArrivedTruck();
+            try {
+                truck = getNextArrivedTruck();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
             if (truck == null) {
                 try {
                     sleep(100);
                 } catch (InterruptedException e) {
                     if (currentThread().isInterrupted()) {
-
+                        Thread.currentThread().interrupt();
                         break;
                     }
                 }
@@ -45,56 +53,72 @@ public class Warehouse extends Thread {
             }
         }
         log.info("Warehouse thread interrupted");
-
     }
 
     private void loadTruck(Truck truck) {
-        log.info("Loading truck {}", truck.getName());
         Collection<Block> blocksToLoad = getFreeBlocks(truck.getCapacity());
+        log.info("Loading into truck {} {} blocks from {}. There are {} blocks left",
+                truck.getName(), blocksToLoad.size(), this.getName(), storage.size());
         try {
             sleep(10L * blocksToLoad.size());
         } catch (InterruptedException e) {
             log.error("Interrupted while loading truck", e);
+            Thread.currentThread().interrupt();
         }
         truck.getBlocks().addAll(blocksToLoad);
-        log.info("Truck loaded {}", truck.getName());
+        log.info("{} loaded", truck.getName());
+        synchronized (truck) {
+            truck.notifyAll();
+        }
     }
 
-    private Collection<Block> getFreeBlocks(int maxItems) {
-        //TODO необходимо реализовать потокобезопасную логику по получению свободных блоков
-        //TODO 1 блок грузится в 1 грузовик, нельзя клонировать блоки во время загрузки
+    private synchronized Collection<Block> getFreeBlocks(int maxItems) {
         List<Block> blocks = new ArrayList<>();
         for (int i = 0; i < maxItems; i++) {
-            blocks.add(new Block());
+            if (storage.isEmpty()) {
+                log.info("no more blocks");
+                break;
+            }
+            blocks.add(storage.remove(0));
         }
         return blocks;
     }
 
-    private void returnBlocksToStorage(List<Block> returnedBlocks) {
-        //TODO реализовать потокобезопасную логику по возврату блоков на склад
+    private synchronized void returnBlocksToStorage(List<Block> returnedBlocks) {
+        storage.addAll(returnedBlocks);
     }
 
     private void unloadTruck(Truck truck) {
-        log.info("Unloading truck {}", truck.getName());
+        log.info("Unloading truck {}, there are {} blocks in it", truck.getName(), truck.getBlocks().size());
         List<Block> arrivedBlocks = truck.getBlocks();
         try {
-            sleep(100L * arrivedBlocks.size());
+            sleep(10L * arrivedBlocks.size());
         } catch (InterruptedException e) {
             log.error("Interrupted while unloading truck", e);
+            Thread.currentThread().interrupt();
         }
         returnBlocksToStorage(arrivedBlocks);
         truck.getBlocks().clear();
-        log.info("Truck unloaded {}", truck.getName());
+        synchronized (truck) {
+            truck.notifyAll();
+        }
+        log.info("{} unloaded at {}, there are {} blocks", truck.getName(), this.getName(), storage.size());
     }
 
-    private Truck getNextArrivedTruck() {
-        //TODO необходимо реализовать логику по получению следующего прибывшего грузовика внутри потока склада
-        return null;
+    private Truck getNextArrivedTruck() throws InterruptedException {
+        return trucks.poll();
     }
-
 
     public void arrive(Truck truck) {
-        //TODO необходимо реализовать логику по сообщению потоку склада о том, что грузовик приехал
-        //TODO так же дождаться разгрузки блоков, при возврате из этого метода - грузовик покинет склад
+        try {
+            trucks.add(truck);
+            synchronized (truck) {
+                truck.wait();
+            }
+            log.info("{} leaving", truck.getName());
+        } catch (InterruptedException e) {
+            log.info("truck arrived interrupted");
+            Thread.currentThread().interrupt();
+        }
     }
 }
